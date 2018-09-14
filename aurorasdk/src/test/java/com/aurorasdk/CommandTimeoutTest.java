39de744e13857c5b36bbe5324f5f6b1cfa5c75f1
Amka78 <amemiyahikaru@bysrsystemdevelopment.com>
2018-09-15 05:42:31 +0900

    Add CommandTimeoutTest

---
+++ aurorasdk/src/test/java/com/aurorasdk/CommandTimeoutTest.java
@@ -0,0 +1,38 @@
+package com.aurorasdk;
+import org.junit.Test;
+import org.mockito.exceptions.base.MockitoException;
+
+import java.util.concurrent.Executor;
+
+import static org.junit.Assert.*;
+public class CommandTimeoutTest {
+
+    @Test
+    public void run_isCorrectIfHandlerIsNull(){
+       CommandTimeout command = new CommandTimeout(null);
+           command.run();
+    }
+
+    @Test
+    public void run_isCorrectIfHandlerExist(){
+        CommandTimeout command = new CommandTimeout(new MockTimeOutHandler());
+        command.run();
+    }
+
+    public class MockTimeOutHandler  implements CommandTimeout.TimeoutHandler{
+
+        @Override
+        public void onTimeout() {
+            throw new  MockException();
+        }
+    }
+
+    public class MockException extends  NullPointerException{
+
+        @Override
+        public void  printStackTrace() {
+            return;
+        }
+
+    }
+}
