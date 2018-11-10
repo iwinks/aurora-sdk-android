137605b1ad64cde74ac6f634ded0dc4cef18073b
Amka78 <amemiyahikaru@bysrsystemdevelopment.com>
2018-11-10 20:56:03 +0900

    Modify LoggerUnitTest line coverage 100%

--- aurorasdk/src/test/java/com/aurorasdk/LoggerTest.java
+++ aurorasdk/src/test/java/com/aurorasdk/LoggerTest.java
@@ -3,25 +3,60 @@ import android.util.Log;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import static org.junit.Assert. *;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
 
 @RunWith(PowerMockRunner.class)
     @PrepareForTest({Logger.class, android.util.Log.class})
     public class LoggerTest {
 
-        @Test(expected=Exception.class)
+        @Test
         public void isLogger_CanOuputErrorLog() {
 
             PowerMockito.mockStatic(android.util.Log.class);
-            PowerMockito.when(Log.e(Mockito.anyString(), Mockito.anyString())).thenThrow(Exception.class);
+            final String[] DebugLogResult = new String[2];
+            PowerMockito.when(Log.e(Mockito.anyString(), Mockito.anyString())).thenAnswer(new Answer<Object>() {
+                public Object answer(InvocationOnMock invocation) throws Throwable {
+                    DebugLogResult[0] = invocation.getArguments()[0].toString();
+                    DebugLogResult[1] = invocation.getArguments()[1].toString();
+
+                    return null;
+                }
+            });
             Logger.e("ErrorLogTest");
+
+            assertEquals("AURORA", DebugLogResult[0]);
+            assertEquals("ErrorLogTest", DebugLogResult[1]);
+        }
+
+        @Test
+        public void isLogger_Exceed4000Word() {
+
+            PowerMockito.mockStatic(android.util.Log.class);
+            final String[] DebugLogResult = new String[2];
+            PowerMockito.when(Log.e(Mockito.anyString(), Mockito.anyString())).thenAnswer(new Answer<Object>() {
+                public Object answer(InvocationOnMock invocation) throws Throwable {
+                    DebugLogResult[0] = invocation.getArguments()[0].toString();
+                    DebugLogResult[1] += invocation.getArguments()[1].toString();
+
+                    return null;
+                }
+            });
+
+            StringBuilder testLogBuilder = new StringBuilder();
+            for(int i = 0; i < 4100; i++){
+                testLogBuilder.append("A");
+            }
+            String testLog = testLogBuilder.toString();
+            Logger.e(testLog);
+
+            assertEquals("AURORA", DebugLogResult[0]);
+            assertTrue(DebugLogResult[1].length() > 4000);
         }
 
         @Test
@@ -30,13 +65,25 @@ import static org.mockito.Mockito.times;
             Logger.w("WarningLogTest");
         }
 
-        @Test(expected=Exception.class)
+        @Test
         public void isLogger_CanOutputDebugLog() {
 
             Logger.setDebug(true);
             PowerMockito.mockStatic(android.util.Log.class);
-            PowerMockito.when(Log.d(Mockito.anyString(), Mockito.anyString())).thenThrow(Exception.class);
-            Logger.d("DegugLogTest");
+
+            final String[] DebugLogResult = new String[2];
+            PowerMockito.when(Log.d(Mockito.anyString(), Mockito.anyString())).thenAnswer(new Answer<Object>() {
+                public Object answer(InvocationOnMock invocation) throws Throwable {
+                    DebugLogResult[0] = invocation.getArguments()[0].toString();
+                    DebugLogResult[1] = invocation.getArguments()[1].toString();
+
+                    return null;
+                }
+            });
+            Logger.d("DebugLogTest");
+
+            assertEquals("AURORA", DebugLogResult[0]);
+            assertEquals("DebugLogTest", DebugLogResult[1]);
         }
     }
 
