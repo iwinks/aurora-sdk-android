bea1320aea0c6cb6fcb8c7cd85b9f7df54ee11a5
Amka78 <amemiyahikaru@bysrsystemdevelopment.com>
2018-11-10 22:19:46 +0900

    Add cofirm getCommandString.

--- aurorasdk/src/test/java/com/aurorasdk/CommandSdFileWriteTest.java
+++ aurorasdk/src/test/java/com/aurorasdk/CommandSdFileWriteTest.java
@@ -7,8 +7,8 @@ public class CommandSdFileWriteTest {
     public void CommandSdFileWrite_isCorrectCreate() {
 
         CommandSdFileWrite constructor1 = new CommandSdFileWrite("testPath", "inputTestValue", true);
-
+        assertEquals("sd-file-write testPath / 1 1 1500 1 3014278670", constructor1.getCommandString());
         CommandSdFileWrite constructor2 = new CommandSdFileWrite("testPath", "testValue");
-
+        assertEquals("sd-file-write testPath / 0 1 1500 1 2229939684", constructor2.getCommandString());
     }
 }
