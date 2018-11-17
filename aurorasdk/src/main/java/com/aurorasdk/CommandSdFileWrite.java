a6ae703dc2f331b07e6d8f4e070662461d88746e
Amka78 <amemiyahikaru@bysrsystemdevelopment.com>
2018-11-18 04:23:56 +0900

    Modify CommandSdFileWrite line & method coverage 100%

--- aurorasdk/src/main/java/com/aurorasdk/CommandSdFileWrite.java
+++ aurorasdk/src/main/java/com/aurorasdk/CommandSdFileWrite.java
@@ -65,7 +65,7 @@ public class CommandSdFileWrite extends Command {
         setInput();
     }
 
-    private void setInput(){
+    protected void setInput(){
 
         try {
             setInput(compressedInput.toByteArray());
